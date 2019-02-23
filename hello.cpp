a5b275353bda787d1c3c08975318c1a782df2b42
Ladislav Martinek <martilad@fit.cvut.cz>
2019-02-23 15:27:25 +0100

    add last print map and how to work with solution

--- hello.cpp
+++ hello.cpp
@@ -1,57 +1,103 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
-void print(vector<vector<int>> array, int m, int n){
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            cout << array[i][j];
-        }
-        cout << endl;
-    }
-}
+enum possibility {
+    empty, l31, l32, l33, l34, l35, l36, l37, l38, l41, l42, l43, l44, l45, l46, l47, l48
+};
+const int PRINT_NUMBERS = 2;
 
-struct cord{
+/**
+ * Coordinate with position.
+ */
+struct cord {
     int x;
     int y;
 };
 
-struct solution{
-    vector<vector<int>> ground;
+/**
+ * Structure to save one solution with map and needed parameters.
+ */
+class solution {
+
+public:
+    int m;
+    int n;
+    vector <vector<int>> ground;
     int price;
     int n_L3;
     int n_L4;
     int n_not;
-    vector<cord> not_cords;
+    vector <cord> not_cords;
+
+    void countEmptyCoords(){
+
+    }
 };
 
-
-int main(){
-	int m, n, k;
-	cin >> m >> n >> k;
-
-	vector<vector<int>> ground = vector<vector<int>>();
-
-	for (int i = 0; i < m; i++){
-	    vector<int> tmp = vector<int>();
-	    for (int j = 0; j < n; j++){
-	        tmp.push_back(0);
-	    }
-        ground.push_back(tmp);
-	}
-
-    for (int i = 0; i < k; i++){
-        int x, y;
-        cin >> x >> y;
-        if (x >= 0 && x < m && y >=0 && y < n){
-            ground[x][y] = 1;
-        }
+/**
+ * Print solution with 2D array and parameter of solution.
+ *
+ * @param array array to print
+ * @param m Number of columns
+ * @param n number of rows
+ */
+void printSolution(solution solution, int m, int n) {
+    cout << "Max price: " << solution.price << endl;
+    cout << "Number of L3: " << solution.n_L3 << endl;
+    cout << "Number of L4: " << solution.n_L4 << endl;
+    cout << "Number of not fill: " << solution.n_not << endl;
+    for (auto &cord : solution.not_cords) {
+        cout << cord.x << " " << cord.y << endl;
     }
 
-	print(ground, m, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m - 1; j++) {
+            cout << setw(PRINT_NUMBERS) << setfill(' ')
+                 << ((solution.ground[j][i] == -1) ? "Z" : (solution.ground[j][i] == 0) ? "-" : to_string(
+                         solution.ground[j][i])) << " ";
+        }
+        cout << setw(PRINT_NUMBERS) << setfill(' ')
+             << ((solution.ground[m-1][i] == -1) ? "Z" : (solution.ground[m-1][i] == 0) ? "-" : to_string(
+                     solution.ground[m-1][i])) << endl;
+    }
+}
 
-	cout << m << endl;
-	cout << n << endl;
-	cout << k << endl;
+
+solution solveMap(vector <vector<int>> array) {
+    solution best = solution();
+
+
+    best.ground = array;
+    return best;
+}
+
+int main() {
+    // load arguments
+    int m, n, k;
+    cin >> m >> n >> k;
+
+    vector <vector<int>> ground = vector < vector < int >> ();
+
+    // init the ground
+    for (int i = 0; i < m; i++) {
+        vector<int> tmp = vector<int>();
+        for (int j = 0; j < n; j++) {
+            tmp.push_back(0);
+        }
+        ground.push_back(tmp);
+    }
+
+    // load the forbidden fields
+    for (int i = 0; i < k; i++) {
+        int x, y;
+        cin >> x >> y;
+        if (x >= 0 && x < m && y >= 0 && y < n) {
+            ground[x][y] = -1;
+        }
+    }
+    printSolution(solveMap(ground), m, n);
 }
