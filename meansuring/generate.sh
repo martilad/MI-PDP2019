facee22176ba47a265221df1425337b468368148
Ladislav Martinek <martilad@fit.cvut.cz>
2019-04-08 22:59:42 +0200

    work on generating sript for test

--- meansuring/generate.sh
+++ meansuring/generate.sh
@@ -7,25 +7,52 @@ mkdir o 2> /dev/null
 mkdir e 2> /dev/null
 
 switch="-mpi"
-nT=20
-nN=20
-nNP=20
+
+nTb=1
+nTm=1
+nTs=1
+
+nNb=20
+nNm=20
+nNs=20
+
+nNPb=20
+nNPm=20
+nNPs=20
+
+repeat=0
 
 for ((startFileN ; startFileN <= $endFileN ; startFileN++)); do
     file=inst/pol${startFileN}.txt
     epath="./o/$startFileN"
     opath="./o/$startFileN"
 
-    startP=1
-    endP=10
-    stepP=2
-    for ((startP ; startP <= $endP ; startP=startP+stepP)); do
-        echo "$startP"
+    nT=$nTb
+    for ((nT ; nT <= $nTm ; nT=nT+nTs)); do
+    	nN=$nNb
+		for ((nN ; nN <= $nNm ; nN=nN+nNs)); do
+			nNP=$nNPb
+			for ((nNP ; nNP <= $nNPm ; nNP=nNP+nNPs)); do
+				rep=0
+				for ((rep ; rep <= $repeat ; rep++)); do
+					# nahrazovani v souboru
+					cp parallel_job_temp.sh parallel_job.sh
+					sed -i "s|#{1}|$switch|g" parallel_job.sh
+					sed -i "s|#{2}|$file|g" parallel_job.sh
+					sed -i "s|#{3}|$nT|" parallel_job.sh
+					sed -i "s|#{4}|$nN|" parallel_job.sh
+					sed -i "s|#{5}|$nNP|" parallel_job.sh
+					sed -i "s|#{6}|$switch|" parallel_job.sh
+					sed -i "s|#{7}|$switch|" parallel_job.sh
+					echo "$nT - $nN - $nNP - $switch - $rep"
+    			done
+    		done
+    	done
     done
 
     echo "Counter: $file"
 done
 
 # nahrazovani v souboru
-cp parallel_job_temp.sh parallel_job.sh
-sed -i 's/#{1}/1/' parallel_job.sh
+#cp parallel_job_temp.sh parallel_job.sh
+#sed -i 's/#{1}/1/' parallel_job.sh
